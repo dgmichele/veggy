422675b1ce5d7285968a52f3447b97fe874b2ccd
Michele del Giudice <dgmichele96@gmail.com>
2025-02-04 10:09:31 +0100

    + .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-// const API_URL = import.meta.env.VITE_API_URL;
-// const API_KEY = import.meta.env.VITE_API_KEY; 
+const API_URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY; 
 
 const api = axios.create({
-  baseURL: 'https://api.spoonacular.com/recipes/', 
+  baseURL: API_URL, 
   params: {
-    apiKey: '7917b03328fa448898672b6e00791541',
+    apiKey: API_KEY,
     diet: 'vegetarian',
     number: 24,
   },
